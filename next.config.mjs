1e55ec51cca4e939cebad33c4dfaf05843563bd6
Taido097 <taido097@gmail.com>
2026-08-10 09:32:32 -0700

    Enable Next image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
   },
 }
 
