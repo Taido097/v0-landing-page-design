92ce5450c2e7cd50cc1ed9e77222d2cb028cd164
Taido097 <taido097@gmail.com>
2026-08-21 14:56:06 -0700

    Stop bypassing Concept 04 page route

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,13 @@
 import { NextRequest, NextResponse } from "next/server"
 
 const NGUYEN_CONCEPT04 = "/client-demos/client-8889/architectured"
-const NGUYEN_CONCEPT04_OPTIMIZED = "/client-demos/client-8889/architectured/optimized"
 
 export function proxy(request: NextRequest) {
-  if (request.nextUrl.pathname !== NGUYEN_CONCEPT04) {
+  if (request.nextUrl.pathname === NGUYEN_CONCEPT04) {
     return NextResponse.next()
   }
 
-  const rewriteUrl = request.nextUrl.clone()
-  rewriteUrl.pathname = NGUYEN_CONCEPT04_OPTIMIZED
-  return NextResponse.rewrite(rewriteUrl)
+  return NextResponse.next()
 }
 
 export const config = {
