58099b5624831e2d9f117a5deb9ef498686688f5
Taido097 <taido097@gmail.com>
2026-08-21 10:36:36 -0700

    Force native Concept 04 route to render fresh

--- app/client-demos/client-8889/architectured/page.tsx
+++ app/client-demos/client-8889/architectured/page.tsx
@@ -1,5 +1,8 @@
 import NativeArchitecturedPage from "./native-page"
 
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 export default function NguyenConcept04() {
   return <NativeArchitecturedPage />
 }
