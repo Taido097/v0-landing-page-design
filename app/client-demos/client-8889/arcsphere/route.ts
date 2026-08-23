c680c1f1a1fb9a1ea5d7be2ca6c4a1164649dd14
Taido097 <taido097@gmail.com>
2026-08-23 16:01:05 -0700

    Redeploy restored Concept 1 build

--- app/client-demos/client-8889/arcsphere/route.ts
+++ app/client-demos/client-8889/arcsphere/route.ts
@@ -1,5 +1,6 @@
 import { renderNguyenPage } from './site';
 
+// Fresh restore commit so Vercel redeploys the last working Concept 1 build.
 export const revalidate = 3600;
 
 export async function GET() {
