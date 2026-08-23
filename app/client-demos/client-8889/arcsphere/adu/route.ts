a563d2e8b0fa06b1e080efcb212b7e5ecf732cba
Taido097 <taido097@gmail.com>
2026-08-23 16:26:57 -0700

    Stabilize ADU preview runtime

--- app/client-demos/client-8889/arcsphere/adu/route.ts
+++ app/client-demos/client-8889/arcsphere/adu/route.ts
@@ -1,7 +1,8 @@
+import { stabilizeFramerPreview } from '../framer-preview';
 import { renderNguyenPage } from '../site';
 
 export const revalidate = 3600;
 
 export async function GET() {
-  return renderNguyenPage('adu');
+  return stabilizeFramerPreview(await renderNguyenPage('adu'));
 }
