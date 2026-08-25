9f29a9d902acfa8a68e45c97da3edea3e57f9bbd
Taido097 <taido097@gmail.com>
2026-08-25 16:05:55 -0700

    Revert broken Card 3 high-res rewrite

--- proxy.ts
+++ proxy.ts
@@ -1,17 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 
 const NGUYEN_CONCEPT04 = "/client-demos/client-8889/architectured"
-const NGUYEN_CARD3_IMAGE = "/nguyen-commercial-building.svg"
-const NGUYEN_CARD3_HIRES = "/client-8889/projects/minimalist-apartment-interior-hires"
 
 export function proxy(request: NextRequest) {
-  if (request.nextUrl.pathname === NGUYEN_CARD3_IMAGE) {
-    const url = request.nextUrl.clone()
-    url.pathname = NGUYEN_CARD3_HIRES
-    url.search = ""
-    return NextResponse.rewrite(url)
-  }
-
   if (request.nextUrl.pathname === NGUYEN_CONCEPT04) {
     return NextResponse.next()
   }
@@ -20,8 +11,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/client-demos/client-8889/architectured",
-    "/nguyen-commercial-building.svg",
-  ],
+  matcher: ["/client-demos/client-8889/architectured"],
 }
