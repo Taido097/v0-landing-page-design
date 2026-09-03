9f7a581c0590dfc9e60225526962eacb6028121d
Taido097 <taido097@gmail.com>
2026-09-03 14:51:51 -0700

    Apply testimonial patch to current Concept 1 production branch

--- app/client-demos/client-8889/arcsphere-socal/route.ts
+++ app/client-demos/client-8889/arcsphere-socal/route.ts
@@ -1,4 +1,5 @@
 import { GET as getConcept } from "../arcsphere-fixed/route"
+import { TESTIMONIAL_PATCH } from "./testimonial-patch"
 
 const OLD_COPY = 'Based in Orange County, we provide commercial architecture, engineering and permit support from existing-condition survey and business layout through plan check and approval.'
 const NEW_COPY = 'Based in Southern California, we provide residential and commercial architecture, engineering, and permit support from concept through approval.'
@@ -383,7 +384,7 @@ export async function GET() {
 
   let html = await response.text()
   html = html.split(OLD_COPY).join(NEW_COPY)
-  html = html.replace('</body>', `${SPLIT_TEXT_PATCH}${BRAND_PATCH}${SQUARE_IMAGES_PATCH}${SERVICES_ANCHOR_PATCH}${MAIN_NAV_PATCH}${ENGINEERING_SERVICE_PATCH}</body>`)
+  html = html.replace('</body>', `${SPLIT_TEXT_PATCH}${BRAND_PATCH}${SQUARE_IMAGES_PATCH}${SERVICES_ANCHOR_PATCH}${MAIN_NAV_PATCH}${ENGINEERING_SERVICE_PATCH}${TESTIMONIAL_PATCH}</body>`)
 
   const headers = new Headers(response.headers)
   headers.set('Content-Type', 'text/html; charset=utf-8')
